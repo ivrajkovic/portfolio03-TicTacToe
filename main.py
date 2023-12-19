23e7afc1cb50204bf9a5401ecbf0f326f68e1752
Ivan Rajković <124137529+ivrajkovic@users.noreply.github.com>
2023-12-19 16:15:34 +0100

    Define Init settings
    Positional init, play init, board print function

---
+++ main.py
@@ -0,0 +1,15 @@
+board_init = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+board_play = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+# Print board game
+def print_board(b):
+    print(f"{b[0]} | {b[1]} | {b[2]}")
+    print("---------")
+    print(f"{b[3]} | {b[4]} | {b[5]}")
+    print("---------")
+    print(f"{b[6]} | {b[7]} | {b[8]}")
+
+
+
+
